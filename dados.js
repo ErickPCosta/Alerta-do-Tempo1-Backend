514a204ae578c01c38cd4761c652041669f88426
Erick Costa <87741743+ErickPCosta@users.noreply.github.com>
2025-05-21 03:01:14 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -21,4 +21,24 @@ const noticias = {
   "Rio de Janeiro": []
 };
 
-module.exports = { alertas, noticias };
+module.exports = {
+  alertas: {
+    "São Paulo": [
+      {
+        event: "Alagamento severo",
+        start: 1716300000,
+        end: 1716321600,
+        description: "Chuvas intensas em toda a região metropolitana."
+      }
+    ]
+  },
+  noticias: {
+    "São Paulo": [
+      {
+        titulo: "Alagamento causa transtornos em SP",
+        link: "https://exemplo.com/noticia1",
+        data: "21/05/2025"
+      }
+    ]
+  }
+};
